cc4cda2f50635f5065f81c5cf55473f2adf362e3
hbx1241 <57489184+hbx1241@users.noreply.github.com>
2022-12-24 20:59:08 +0800

    Change Directory Of mqtt_subscriber.py

--- server/mqtt_subscriber.py
+++ server/mqtt_subscriber.py
@@ -1,5 +1,3 @@
-# new version on Rpi
-
 import random
 import RPi.GPIO as GPIO
 from paho.mqtt import client as mqtt_client
@@ -30,12 +28,24 @@ def connect_mqtt() -> mqtt_client:
 
 def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
-        light_on = msg.payload.decode()
-        print(f"Received `{light_on}` from `{msg.topic}` topic")
-        if light_on == 'True':
-            GPIO.output(11,True)
-        elif light_on == 'False':
-            GPIO.output(11,False)
+        topic = msg.topic
+        msg = msg.payload.decode()
+        light_idx = msg[0]
+        light_on = msg[1:]
+        print(light_idx, light_on)
+        print(f"Received `{msg}` from `{topic}` topic")
+        if light_idx == '1':
+            if light_on == 'True':
+                GPIO.output(11,True)
+            elif light_on == 'False':
+                GPIO.output(11,False)
+        elif light_idx == '2':
+            if light_on == 'True':
+                GPIO.output(15,True)
+            elif light_on == 'False':
+                GPIO.output(15, False)
+
+
         
     client.subscribe(topic)
     client.on_message = on_message
@@ -44,7 +54,10 @@ def subscribe(client: mqtt_client):
 def run():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(11, GPIO.OUT)    # 將P1接頭的11腳位設定為輸出
-    GPIO.output(11, True)    # turn on the light initially
+    GPIO.output(11, True)   # turn on the light initially
+    GPIO.setup(15, GPIO.OUT)
+    GPIO.output(15, True)
+
     client = connect_mqtt()
     subscribe(client)
     client.loop_forever()
